e6b8b3a125e3db250f8cce00f3550eaf3c0784ea
Sission <cao.schen@gmail.com>
2021-11-03 10:50:11 -0400

    Add example figures

--- README.md
+++ README.md
@@ -6,23 +6,25 @@ It is a stock monitoring script. This repository is still under developing.
 
 ## Getting Started
 
-### Dependencies
-
-* selenium
-
-### Installing
+### Prerequisites & Installing
 
 ```
+pip install selenium
 git clone https://github.com/Sission/StockMonitoring.git
 ```
-### Executing program
+Add the url of your desired item in to *bestbuy_url.txt*
 
+### Executing program
 ```
 python run bestbuy_main.py
 ```
+### Example
+
+![Output]<img src="Examples/result_figure.png">
 
 ### Future Functions
  - Monitor other websites such as Newegg.com
  - Send notification via text or Webhook
  - Pay for the item automatically 
+ - Docker package
  
--- bestbuy_main.py
+++ bestbuy_main.py
@@ -2,6 +2,8 @@ import os
 from gpu_restock.bestbuy_stock import StockChecking
 import time
 
+TESTING = False
+
 url_root = os.path.abspath(os.path.dirname(__file__))
 url_path = os.path.join(url_root, "gpu_restock/bestbuy_url.txt")
 
@@ -13,25 +15,29 @@ bots = []
 for i in range(len(url)):
     bots.append(StockChecking(url=url[i]))
 
-start_time = time.time()
-seconds = 15
+if TESTING:
+    start_time = time.time()
+    seconds = 15
 
 try:
     for bot in bots:
         bot.load_page()
 
     while True:
-        current_time = time.time()
-        elapsed_time = current_time - start_time
 
         for bot in bots:
-            time.sleep(2)
+            time.sleep(5)
             bot.refresh()
             bot.run()
             # bot.check()
-        if elapsed_time > seconds:
-            print("Finished iterating in: " + str(int(elapsed_time)) + " seconds")
-            break
+
+        if TESTING:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+
+            if elapsed_time > seconds:
+                print("Finished iterating in: " + str(int(elapsed_time)) + " seconds")
+                break
 
 except Exception as e:
     raise
