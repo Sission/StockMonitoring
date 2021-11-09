efb9fef6a35a2ee86ac4cdddc3a32e89455f6f7b
Sission <cao.schen@gmail.com>
2021-11-08 19:32:31 -0500

    added gitignore

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8.5
-ADD . /code
-WORKDIR /code
-RUN pip install -r requirements.txt
-CMD ["python", "main.py"]
--- test.py
+++
@@ -1,7 +0,0 @@
-# Schedule Library imported
-import schedule
-import time
-from datetime import datetime
-
-if datetime.now().hour > 8 and ((datetime.now().hour + 1) % 2):
-    print(1)
